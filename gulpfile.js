5a28c354d8f18b4e22b0beb47685713e71295fdc
Bo Kamphues <b.kamphues@pm.me>
2022-01-13 12:51:32 +0100

    Added basic gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-function defaultTask(cb) {
-    cb();
-}
+// Import src and dest from gulp.
+const { src, dest } = require("gulp");
 
-exports.default = defaultTask
+// Import gulp plugins.
+const babel = require("gulp-babel");
+
+exports.default = function (done) {
+  return src("./src/components/**/*.js")
+    .pipe(
+      babel({
+        presets: [
+          [
+            "@babel/env",
+            {
+              modules: false,
+            },
+          ],
+        ],
+      })
+    )
+    .pipe(dest("./dist"));
+};
