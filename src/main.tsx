19cd139dca0430297a95f6d7f3f5e43a9d348eb4
Bo Kamphues <b.kamphues@pm.me>
2022-01-16 15:20:27 +0100

    Added basic css

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "normalize.css";
+import "./styles/sakura.css";
+import "./styles/main.scss";
 
 ReactDOM.render(<h1>Woordle - Woorden!</h1>, document.getElementById("root"));
